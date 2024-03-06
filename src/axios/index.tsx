a730c39713baf2854b62d48b7ead6d85d923d23d
diephuy2105vn <tuanhuy2005vn@gmail.com>
2024-03-06 20:50:41 +0700

    Cap nhat lai axios

--- src/axios/index.tsx
+++ src/axios/index.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     withCredentials: true,
-    baseURL: "http://localhost:8080/api/",
+    baseURL: "https://feaneserver-production.up.railway.app/api/",
 });
 
 export default instance;
